666771abd263b35d517cd2cbb0d4aae307589534
Jonathan García <megabyte9703@gmail.com>
2022-03-27 10:01:43 -0600

    First template

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <>
+      <h1>Lista de notas</h1>
+    </>
+  )
+}
+
+export default Main
---
+++ frontend/src/pages/View.js
@@ -0,0 +1,9 @@
+function View() {
+  return (
+    <>
+      <h1>Ver nota</h1>
+    </>
+  )
+}
+
+export default View
