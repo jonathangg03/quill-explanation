c65fc8243c730237159dd53d4400ac5591d856a7
Jonathan García <megabyte9703@gmail.com>
2022-03-27 11:10:12 -0600

    Test functionallity

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,11 +1,51 @@
+import { useState } from 'react'
+import { useQuill } from 'react-quilljs'
+import { useNavigate } from 'react-router-dom'
+import toolbar from '../toolbar'
+import 'quill/dist/quill.snow.css'
+
 function New() {
+  const [title, setTitle] = useState('')
+  const { quill, quillRef } = useQuill({ modules: { toolbar: toolbar } })
+  const navigate = useNavigate()
+
+  const handleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const data = {
+      title: title,
+      content: JSON.stringify(quill.getContents())
+    }
+    try {
+      await fetch('http://localhost:3001/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      navigate('/')
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <>
       <h1>Nueva nota</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='title'>Título:</label>
-        <input type='text' placeholder='titulo' id='value' />
-        <div className='editor'></div>
+        <input
+          type='text'
+          placeholder='titulo'
+          id='value'
+          value={title}
+          onChange={handleChange}
+        />
+        <div className='editor' ref={quillRef}></div>
         <button>Enviar</button>
       </form>
     </>
